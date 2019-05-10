2af5cbf47b08a7daa59d11ee6ba122220a2c80d3
Pranay Irdande <pranay.irdande@amla.io>
2019-05-10 10:23:47 +0530

    Changes in global variable

---
+++ utilities/commonUtility/GlobalVariable.java
@@ -0,0 +1,14 @@
+package commonUtility;
+
+public class GlobalVariable {
+
+	public static final int DelayVeryLow = 2;
+	
+	public static final int DelayLow = 5;
+	
+	public final static int DelayMedium = 10;
+	
+	public static final int DelayHigh = 15;
+	
+	public static final int DelayVeryHigh = 20;
+}
